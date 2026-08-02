e98c0762297857fad92c845cb4fd057a0370d6db
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 15:39:59 +0300

    Solution 1

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function makeCounter() {
     // Use a let binding inside this function and return a function
+    let count = 0;
     // that increments and returns it.
+
+    return function () {
+        count++;
+        return count
+    }
 }
 
 const n = Number(require('fs').readFileSync(0, 'utf-8').trim());
