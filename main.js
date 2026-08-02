8e0c9d163ee444faefaa1a033034d6164f890fe1
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-02 20:13:20 +0300

    Solution 3

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-function makeAdder(x) {
-    return function (y) {
-        return y + x;
-    }
-}
-
-
 process.stdin.on("data", (chunk) => {
     const data = chunk.toString().trim().split("\n").map(item => Number(item));
 
-    const add = makeAdder(data[0]);
-    console.log(add(data[1]))
+    const counterNumber = (function () {
+        let counter = 0;
+
+        return {
+            increment() {
+                counter++;
+            },
+
+            value() {
+                return counter
+            }
+        }
+    })();
+
+    for (let i = 0; i < data[0]; i++) {
+        counterNumber.increment();
+    }
+
+    console.log(counterNumber.value())
 })
