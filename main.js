547d7d169e8c23d33feec1831671fc6145a3ee34
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-04 17:48:19 +0300

    Solution 5

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const promise = new Promise((resolve, reject) => {
-    resolve("hello");
-})
+process.stdin.on("data", chunk => {
+    const inputData = chunk.toString().trim().split("\n").map(item => Number(item));
 
-promise.then(console.log);
+    const promise1 = new Promise((resolve, reject) => {
+        resolve(inputData[0]);
+    })
+
+    const promise2 = new Promise((resolve, reject) => {
+        resolve(inputData[1]);
+    });
+
+
+    async function main() {
+        const gatherNumbers = await Promise.all([promise1, promise2]);
+        const sum = gatherNumbers.reduce((acc, item) => acc + item, 0);
+        console.log(sum);
+    }
+    main()
+})
