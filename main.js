694a025aa930a4c8c97061a43cdc68e7d13e6036
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-04 17:32:03 +0300

    Solution 4

--- main.js
+++ main.js
@@ -1,23 +1,5 @@
-process.stdin.on("data", (chunk) => {
-    const data = chunk.toString().trim().split("\n").map(item => Number(item));
-
-    const counterNumber = (function () {
-        let counter = 0;
-
-        return {
-            increment() {
-                counter++;
-            },
-
-            value() {
-                return counter
-            }
-        }
-    })();
-
-    for (let i = 0; i < data[0]; i++) {
-        counterNumber.increment();
-    }
-
-    console.log(counterNumber.value())
+const promise = new Promise((resolve, reject) => {
+    resolve("hello");
 })
+
+promise.then(console.log);
