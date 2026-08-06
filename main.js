4c9fed8285c2099cb830d4858e186c2b5fa5f2aa
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-06 16:22:30 +0300

    Solution 13

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
 process.stdin.on("data", chunk => {
     // reading name from standard input/output
-    const inputData = chunk.toString().trim();
+    const inputData = chunk.toString().trim().split(/\s/g);
 
-    const result = inputData.match(/[0-9]/g);
-    // const num = result.reduce((acc, item) => acc++, 0);
-    console.log(result ? result.length : 0);
+    const wordsCounter = {};
+
+    // add count of words
+    for( let word of inputData) {
+        if(!wordsCounter[word]) {
+            wordsCounter[word] = 0;
+        }
+
+
+        let item = wordsCounter[word];
+        item++;
+        wordsCounter[word] = item;
+    }
+
+    // sort Object and let it iterable
+    const sortedWordsCounter = new Map(Object.entries(wordsCounter).sort());
+
+    // print output
+    for ( let [key, value] of sortedWordsCounter ) {
+        console.log(`${key}: ${value}`);
+    }
     
 })
