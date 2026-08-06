3213c74a2ae9fa85003bdea698093eaa6fc5329d
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-06 15:56:15 +0300

    Solution 12

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
 process.stdin.on("data", chunk => {
     // reading name from standard input/output
-    const inputData = chunk.toString().trim().split("\n").map(item => Number(item));
+    const inputData = chunk.toString().trim();
 
-    
-
-    function* fib(n) {
-
-        let startNum = 0;
-        let lastNum = 1;
-        let nextNum = 0;
-
-        for (let i = 0; i < n; i++) {
-            yield startNum;
-
-            nextNum = startNum + lastNum;
-            startNum = lastNum;
-            lastNum = nextNum;
-        }
-        
-    }
-
-    const generator = fib(inputData[0]);
-    for (let i = 0; i < inputData[0]; i++) {
-        console.log(generator.next().value)
-    }
+    const result = inputData.match(/[0-9]/g);
+    // const num = result.reduce((acc, item) => acc++, 0);
+    console.log(result ? result.length : 0);
     
 })
