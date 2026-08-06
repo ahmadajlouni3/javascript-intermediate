9fccc63057ea3e03aa9990e63894184a93fbf590
ahmadajlouni3 <bootsetup1@gmail.com>
2026-08-06 17:18:41 +0300

    Solution 15

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-
-
-class Person {
-    constructor(firstname, lastname) {
-        this._first = firstname;
-        this._last = lastname;
-    }
-
-    get firstname() {return this._first};
-    set firstname(name) {this._first = name};
-
-    get lastname() {return this._last};
-    set lastname(name) {this._last = name};
-
-    get fullname() {return `${this._first} ${this._last}`};
-    set fullname(name) {
-        const fullnameParam = name.trim().split(/\s/g);
-        this._first = fullnameParam[0];
-        this._last = fullnameParam[1];
-    }
-}
-
-const person = new Person("Ada", "Lovelace");
-console.log(person.fullname);
-person.firstname = "Linus";
-console.log(person.firstname);
-person.lastname = "Torvalds"
-console.log(person.lastname);
+console.log("import()");
+console.log(".mjs");
+console.log("import.meta.url");
